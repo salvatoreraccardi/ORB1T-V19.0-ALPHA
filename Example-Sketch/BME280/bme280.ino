f604b4d290907edac36b8cb309f76f85ccef818d
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2020-03-04 10:45:24 +0100

    Create bme280.ino

---
+++ Example-Sketch/BME280/bme280.ino
@@ -0,0 +1,53 @@
+/*
+ * OBJEX-IOT.TECH
+ * SKETCH FOR ORB1T V19.0 ALPHA
+ */
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BME280 test"));
+
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  
+  // Wait calibration
+  delay(500);
+}
+
+void loop() {
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+    delay(2000);
+}
